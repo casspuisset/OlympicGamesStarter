ab81555770e253b47e3c7fb792a6b2fb0ff54866
Cassandre Puisset <cpuisset98@gmail.com>
2025-04-17 11:39:34 +0200

    interfaces

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,9 +1,12 @@
-// TODO: create here a typescript interface for an olympic country
-/*
-example of an olympic country:
-{
-    id: 1,
-    country: "Italy",
-    participations: []
+// Interface for olympic country
+import { OlympicParticipation } from './Participation';
+
+export interface OlympicCountry {
+  id: number;
+  country: string;
+  participations: [
+    {
+      OlympicParticipation: OlympicParticipation;
+    }
+  ];
 }
-*/
